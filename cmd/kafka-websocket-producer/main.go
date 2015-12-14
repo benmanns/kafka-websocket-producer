08b264f73bb3d81eeb40277b5a6d6e32842039d4
Benjamin Manns <benmanns@gmail.com>
2015-12-14 17:24:00 -0500

    Fix formatting.

--- cmd/kafka-websocket-producer/main.go
+++ cmd/kafka-websocket-producer/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "flag"
+	"flag"
 	"fmt"
 	"log"
 
@@ -11,32 +11,32 @@ import (
 var url = flag.String("url", "ws://localhost:8989/ws", "websocket address")
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    log.Println("connecting to:", *url)
-    c, _, err := websocket.DefaultDialer.Dial(*url, nil)
-    if err != nil {
-        log.Fatalln("dial:", err)
-    }
-    defer func() {
-        if err := c.Close(); err != nil {
-            fmt.Println("close:", err)
-            return
-        }
-    }()
-    defer func() {
-        if err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "")); err != nil {
-            log.Println("write close:", err)
-            return
-        }
-    }()
+	log.Println("connecting to:", *url)
+	c, _, err := websocket.DefaultDialer.Dial(*url, nil)
+	if err != nil {
+		log.Fatalln("dial:", err)
+	}
+	defer func() {
+		if err := c.Close(); err != nil {
+			fmt.Println("close:", err)
+			return
+		}
+	}()
+	defer func() {
+		if err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "")); err != nil {
+			log.Println("write close:", err)
+			return
+		}
+	}()
 
-    for {
-        _, message, err := c.ReadMessage()
-        if err != nil {
-            log.Println("read:", err)
-            return
-        }
-        fmt.Println(string(message))
-    }
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			return
+		}
+		fmt.Println(string(message))
+	}
 }
